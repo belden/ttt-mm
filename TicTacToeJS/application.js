4e2745ecf878c85fc7d22ae18315708fb3b2907e
fokusfm@yandex.ru <fokusfm@yandex.ru>
2017-08-02 23:41:14 +0300

    Initial

---
+++ TicTacToeJS/application.js
@@ -0,0 +1,24 @@
+class Cell {
+	constructor() {
+		this.state = 'X';
+	}
+}
+
+var cellArray = [
+	new Cell(),
+	new Cell(),
+	new Cell(),
+	new Cell(),
+	new Cell(),
+	new Cell(),
+	new Cell(),
+	new Cell(),
+	new Cell()
+];
+
+function render() {
+	//$('.cell')
+
+}
+
+

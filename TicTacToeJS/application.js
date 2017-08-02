b877ba420fa2fc77323944b7fdca0a776d4ab8d2
fokusfm@yandex.ru <fokusfm@yandex.ru>
2017-08-03 00:03:27 +0300

    * Implement set state

--- TicTacToeJS/application.js
+++ TicTacToeJS/application.js
@@ -1,16 +1,41 @@
+var currentPlayer = 'X';
+
+
 class Cell {
 	constructor() {
-		this.state = 'X';
+		this.state = null;
+	}
+
+	setState(value) {
+		if (this.state) {
+			return false;
+		}
+
+		this.state = value;
+		return true;
 	}
 }
+
+
+
 
 init();
 render();
 
+function changeThePlayer() {
+	currentPlayer = currentPlayer === 'X' ? 'Y' : 'X';
+}
 
 function init() {
 	$('.cell').each(function (cell) {
 		$(this).data('cell', new Cell());
+		$(this).click(function () {
+			if ($(this).data('cell').setState(currentPlayer)) {
+				changeThePlayer();
+				render();
+			}
+
+		});
 	});
 
 }

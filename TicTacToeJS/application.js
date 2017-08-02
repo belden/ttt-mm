0777ba67b3600fb51d01ae84ac31933bcc6bdcd3
fokusfm@yandex.ru <fokusfm@yandex.ru>
2017-08-02 23:50:48 +0300

    * Initial state of the game * Render function

--- TicTacToeJS/application.js
+++ TicTacToeJS/application.js
@@ -4,21 +4,19 @@ class Cell {
 	}
 }
 
-var cellArray = [
-	new Cell(),
-	new Cell(),
-	new Cell(),
-	new Cell(),
-	new Cell(),
-	new Cell(),
-	new Cell(),
-	new Cell(),
-	new Cell()
-];
+init();
+render();
 
-function render() {
-	//$('.cell')
+
+function init() {
+	$('.cell').each(function (cell) {
+		$(this).data('cell', new Cell());
+	});
 
 }
 
-
+function render() {
+	$('.cell').each(function (cell) {
+		$(this).text($(this).data('cell').state);
+	});
+}
